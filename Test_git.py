0dff2cded6c61d5caef7dd9ac797af3012684883
Mikhail Laushuk <mikhail.laushuk@LeverX.local>
2023-10-08 02:37:56 +0300

    Питоновский комит

---
+++ Test_git.py
@@ -0,0 +1,6 @@
+first_name = input("Please enter a name: ")
+src_loc = input("Please enter a start: ")
+dest_loc = input("Please enter a destination: ")
+mot = input("Please enter a means of transportation: ")
+
+print( first_name, "wants to travel from", src_loc, "to", dest_loc, "by", mot )
